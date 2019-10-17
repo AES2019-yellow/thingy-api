c07a142283d2d56faad1c0054a3fba92f5641698
Yi Zhang <yi.zhang@unifr.ch>
2019-10-18 00:52:53 +0200

    async mqtt added and tested well

--- test-mqtt.js
+++ test-mqtt.js
@@ -1,4 +1,13 @@
-const mqtt = require('mqtt');
+/*
+ * File: /test-mqtt.js
+ * Project: thingy-api-yellow
+ * File Created: Thu, 17th October 2019 3:08:14 pm
+ * Author: Yi Zhang (yi.zhang@unifr.ch)
+ * -----
+ * Copyright 2019 - 2019 AES-Unifr, AES2019-Yellow
+ */
+
+const mqtt = require("async-mqtt") // changed to async lib, canbe then integrated in koa
 const host = "mqtt://mqtt.thing.zone";
 const options = {
     port : 1896,
@@ -7,16 +16,26 @@ const options = {
     topic: '#',
 };
 
+const client = mqtt.connect(host,options);
 
-var client = mqtt.connect(host,options);
+const doSub = async () => {
+    console.log("Starting...");
+    try {
+        await client.subscribe('#').then(displaySub);
+    } catch (e) {
+        console.log(e.stack);
+        process.exit();
+        
+    }
+}
 
-client.on('connect',()=>{
-    client.subscribe('#', (err)=>{
-        if (!err) {
-            client.on('message', (topic, message, packet)=>{
-                console.log("[Topic]: '"+topic+"' | [Message]: '"+ message + "'");
-            });
-        }
-    });
-});
+const displaySub = () => {
+        client.on('message',(topic,message,packet)=>{
+            // @Todo serialization 
+            console.log("[Topic]: '"+ topic +"' | [Message]: '"+ message + "'"); 
+        });
+    }
+
+    client.on("connect",doSub);
+
 
