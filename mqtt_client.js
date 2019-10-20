fdf12a749da9d9924d9fa636d765031bdb5fed59
Yi Zhang <yi.zhang@unifr.ch>
2019-10-20 23:18:04 +0200

    mqtt-client with parser to redis is done

--- mqtt_client.js
+++ mqtt_client.js
@@ -1,4 +1,4 @@
-const mqtt = require("async-mqtt") // changed to async lib, canbe then integrated in koa
+const mqtt = require("async-mqtt") // changed to async lib, can be then integrated in koa
 const host = "mqtt://mqtt.thing.zone";
 const options = {
     port : 1896,
@@ -33,6 +33,7 @@ SubModel.prototype.dispatcher = function (topic, message, packet) {
             'Thingy Humidity Characteristic': 'humidity',
             'Thingy Light Intensity Characteristic': 'lightIntensity',
             
+            // Services below belong to the 
             // 'Thingy LED Characteristic':'led',
             // 'Thingy Button Characteristic':'button',
 
@@ -40,12 +41,10 @@ SubModel.prototype.dispatcher = function (topic, message, packet) {
         };
         var topic_type = env_types[env_type];
         // console.log(sub[topic_type]);
-        if (topic_type == 'temperature'){
+        if (topic_type != 'otherEnvServices'){
             return sub[topic_type](thingy_id,topic_type,message,packet);
-
         }
     }
-
 }
 
 SubModel.prototype.temperature = async (thingy_id,topic_type,message, packet) => {
@@ -54,18 +53,57 @@ SubModel.prototype.temperature = async (thingy_id,topic_type,message, packet) =>
     let temperature_arr = message.toString().split(',');
     let temperature_val = parseFloat(temperature_arr[0]+"."+temperature_arr[1]);
     let key = thingy_id+':'+topic_type;
-    res = await redis.xadd(key,'*','temperature',temperature_val.toString());
+    res = await redis.xadd(key,'*',topic_type,temperature_val,'timestamp',timestamp);
     console.log(res);
     return res; // key entry returns if successful
-
 }
 
-SubModel.prototype.pressure = function (thingy_id,topic,message,packet) {
-
+SubModel.prototype.pressure = async function (thingy_id,topic_type,message,packet) {
+    let res = false;
+    let timestamp = getTime();
+    let pressure_arr = message.toString().split(',');
+    let pressure_val = parseFloat(pressure_arr[0]+"."+pressure_arr[1]);
+    let key = thingy_id+':'+topic_type;
+    res = await redis.xadd(key,'*',topic_type,pressure_val,'timestamp',timestamp);
+    console.log(res);
+    return res; // key entry returns if successful
 }
 
-SubModel.prototype.airQuality = function (thingy_id,topic,message,packet) {
+SubModel.prototype.airQuality = async function (thingy_id,topic_type,message,packet) {
+    let res = false;
+    let timestamp = getTime();
+    let topic_types = ['CO2','TVOC'];
+    let airQ_arr = message.toString().split(',');
+    let co2_val = parseInt(airQ_arr[0]);
+    let voc_val = parseInt(airQ_arr[1]);
+    let key = thingy_id+':'+topic_type;
+    res = await redis.xadd(key,'*',topic_types[0],co2_val,topic_types[1],voc_val,'timestamp',timestamp);
+    console.log(res);
+    return res; // key entry returns if successful 
+}
 
+SubModel.prototype.humidity = async function(thingy_id,topic_type,message,packet){
+    let res = false;
+    let timestamp = getTime();
+    let humidity_val = parseInt(message.toString());
+    let key = thingy_id+':'+topic_type;
+    res = await redis.xadd(key,'*',topic_type,humidity_val,'timestamp',timestamp);
+    console.log(res);
+    return res; // key entry returns if successful
+}
+
+SubModel.prototype.lightIntensity = async function(thingy_id,topic_type,message,packet){
+    let res = false;
+    let timestamp = getTime();
+    let light_vals = message.toString().split(',');
+    let light_types = ['R','G','B','A']
+    let key = thingy_id+':'+topic_type; 
+    res = await redis.xadd(key, '*', 
+    light_types[0],light_vals[0],
+    light_types[1],light_vals[1],
+    light_types[2],light_vals[2],
+    light_types[3],light_vals[3],
+    'timestamp',timestamp)
 }
 
 var getTime = () =>{
@@ -92,7 +130,7 @@ const onMessage = () => {
         exports.client.on('message',(topic,message,packet)=>{
             
             // @Todo serialization 
-            // console.log("[Topic]: '"+ topic +"' | [Message]: '"+ message + "'");
+            console.log("[Topic]: '"+ topic +"' | [Message]: '"+ message + "'");
             let res = sub.dispatcher(topic,message,packet);
         });
     }
